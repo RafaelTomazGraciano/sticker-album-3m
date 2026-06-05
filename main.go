17b472930de4c2219e081ab82b5d2053870599eb
Rafael Tomaz Graciano <rafaeltomaz2005@gmail.com>
2026-06-05 12:43:29 -0300

    feat: flag id

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main(){
+	// Flag id
+	idPtr := flag.Int("id", 0, "ID númerico do aluno") 
+	flag.Parse()
+
+	if *idPtr <= 0{
+		fmt.Println("Erro: Você precisa informar um ID de aluno válido e maior que zero.")
+		fmt.Println("Use o parâmetro -id. Exemplo: go run . -id 5")
+		os.Exit(1)
+	}
+
+	idAluno := *idPtr
+	peerID := fmt.Sprintf("ALUNO-%02d", idAluno)
+	stickerID := fmt.Sprintf("FIG-%02d", idAluno)
+
+	// Web Socket
 	http.HandleFunc("/ws", wsHandler)
 	fmt.Println("WebSocket server started on :8080")
 	err := http.ListenAndServe(":8080", nil)
